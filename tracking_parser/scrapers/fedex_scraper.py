da9b63ac3c109ad45326d16d89a6c73cd621d2f5
Shihab Sarar <shihab312417@gmail.com>
2025-05-19 19:55:42 +0600

    Update fedex_scraper.py

--- tracking_parser/scrapers/fedex_scraper.py
+++ tracking_parser/scrapers/fedex_scraper.py
@@ -43,7 +43,7 @@ class FedExScraper(BaseScraper):
             try:
                 with sync_playwright() as p:
                     browser = p.chromium.launch(
-                        headless=False,
+                        headless=True,
                         args=[
                             "--disable-blink-features=AutomationControlled",
                             "--no-sandbox",
